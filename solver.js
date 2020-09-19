d342fc4801d64b13e4c9912c37559b791f4e5fa5
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-09-19 15:12:11 +0200

    first commit

---
+++ solver.js
@@ -0,0 +1,78 @@
+const _board = [
+    ['8', '', '', '', '', '', '', '', ''],
+    ['', '', '3', '6', '', '', '', '', ''],
+    ['', '7', '', '', '9', '', '2', '', ''],
+    ['', '5', '', '', '', '7', '', '', ''],
+    ['', '', '', '', '4', '5', '7', '', ''],
+    ['', '', '', '1', '', '', '', '3', ''],
+    ['', '', '1', '', '', '', '', '6', '8'],
+    ['', '', '8', '5', '', '', '', '1', ''],
+    ['', '9', '', '', '', '', '4', '', ''],
+];
+
+function wait(milliseconds) {
+    if(milliseconds == 0) {
+        return true;
+    }
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
+function checkBoard(data, row, col, k) {
+    for(var i = 0; i < 9; i++) {
+        if(data[i][col] == k || data[row][i] == k || data[3 * Math.floor(row / 3) + Math.floor(i / 3)][3 * Math.floor(col / 3) + i%3] == k) {
+            return false;
+        }
+    }
+    return true;
+}
+
+async function sudokuSolver(data, time) {
+    for(let i = 0; i < 9; i++) {
+        for(let j = 0; j < 9; j++) {
+            if(data[i][j] == '') {
+                for(let k = 1; k <= 9; k++) {
+                    if(await checkBoard(data, i, j, k)) {
+                        data[i][j] = `${k}`;
+                        await wait(time);
+                        document.getElementById(i+':'+j).value = `${k}`;
+                        if(await sudokuSolver(data, time)) {
+                            return true;
+                        } else {
+                            data[i][j] = '';
+                            document.getElementById(i+':'+j).value = ``;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function start() {
+    for(var i = 0; i < 9; i++) {
+        for(var j = 0; j < 9; j++) {
+            _board[i][j] = document.getElementById(i+':'+j).value;
+        };
+    }; 
+    var time = document.getElementById('timeMs').value;
+    sudokuSolver(_board, time);
+};
+
+var htmlifiedBoard = "";
+for(var i = 0; i < 9; i++) {
+    for(var j = 0; j < 9; j++) {
+        htmlifiedBoard += '<input type="text" id="' + i + ':' + j + '" value="' + _board[i][j] + '">';
+        if(j == 8) {
+            htmlifiedBoard += '<br>';
+            if(i%3 == 2) {
+                htmlifiedBoard += '<br>';
+            }
+        }
+        if(j%3 == 2) {
+            htmlifiedBoard += ' ';
+        }
+    };
+};
+document.getElementById('grid').innerHTML = htmlifiedBoard;
